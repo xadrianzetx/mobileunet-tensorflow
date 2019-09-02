2bd3db01bece32424c83fa345a9f9f2a9af44cee
Adrian Zuber <xadrianzetx@gmail.com>
2019-09-02 20:10:35 +0200

    DataGenerator returns batches of img-mask pairs now

--- scnn/dataset.py
+++ scnn/dataset.py
@@ -1,15 +1,18 @@
 import os
+import cv2
 import numpy as np
 import pandas as pd
+from PIL import Image
 from utils.decoding import mask_from_splines
 
 
 class ImageDataGenerator:
 
-    def __init__(self, path, batch_size, lookup_name):
+    def __init__(self, path, batch_size, lookup_name, augment=False):
         self._path = path
         self._batch_size = batch_size
         self._lookup_name = lookup_name
+        self._augment = augment
         self._lookup = self._read_lookup()
         self._max_idx = self._lookup.shape[0]
 
@@ -29,13 +32,35 @@ class ImageDataGenerator:
 
         batch = self._lookup.loc[self.idx:self.idx + self._batch_size - 1]
         self.idx += self._batch_size
-        x = []
+        batch_x, batch_y = [], []
 
         for idx, row in batch.iterrows():
-            img_path = row['img_path'].replace('/', '\\')
-            x.append(os.path.join(self._path, img_path))
+            # load image and switch color channels
+            img_path = row['img_path'].replace('/', os.path.sep)
+            img = Image.open(os.path.join(self._path, img_path[1:]))
+            img = np.array(img)
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            height, width, _ = img.shape
 
-        return x
+            # create empty mask
+            mask_path = os.path.splitext(row['img_path'])[0]
+            mask_path += '.lines.txt'
+            mask_path = mask_path.replace('/', os.path.sep)
+            mask = np.zeros((height, width), dtype=int)
+
+            with open(os.path.join(self._path, mask_path[1:]), 'r') as file:
+                # add lines to mask one by one
+                for line in file:
+                    mask = mask_from_splines(line=line, mask=mask)
+
+            if self._augment:
+                # TODO
+                self._augment_image_mask(img, mask)
+
+            batch_x.append(img)
+            batch_y.append(mask)
+
+        return np.array(batch_x), np.array(batch_y)
 
     def _read_lookup(self):
         abs_path = os.path.join(self._path, self._lookup_name)
@@ -48,5 +73,8 @@ class ImageDataGenerator:
 
         return lookup.sample(frac=1).reset_index(drop=True)
 
-    def create_mask(self):
+    def _create_mask(self, size, splines_path):
+        pass
+
+    def _augment_image_mask(self, img, mask):
         pass
