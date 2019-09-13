a363d81c8a337536d04778b9ce6cf029db7f8625
Adrian Zuber <xadrianzetx@gmail.com>
2019-09-13 20:10:42 +0200

    re-implemented losses

--- scnn/losses.py
+++ scnn/losses.py
@@ -1,27 +1,41 @@
 import tensorflow.keras.backend as K
 
 
-def focal_loss(y_true, y_pred, alpha=0.8, gamma=2):
-    y_true = K.flatten(y_true)
-    y_pred = K.flatten(y_pred)
+class FocalLoss:
 
-    bce = K.binary_crossentropy(y_true, y_pred)
-    bce_exp = K.exp(-bce)
-    loss = K.mean(alpha * K.pow((1 - bce_exp), gamma) * bce)
+    def __init__(self, alpha, gamma):
+        self.alpha = alpha
+        self.gamma = gamma
+    
+    def __call__(self, y_true, y_pred, sample_weight=None):
+        y_true = K.flatten(y_true)
+        y_pred = K.flatten(y_pred)
 
-    return loss
+        bce = K.binary_crossentropy(y_true, y_pred)
+        bce_exp = K.exp(-bce)
+        loss = K.mean(self.alpha * K.pow((1 - bce_exp), self.gamma) * bce)
+
+        return loss
 
 
-def focal_tversky_loss(y_true, y_pred, alpha, beta, gamma, smooth):
-    y_true = K.flatten(y_true)
-    y_pred = K.flatten(y_pred)
+class FocalTverskyLoss:
 
-    # true pos, false pos, false neg
-    tp = K.sum((y_pred * y_true))
-    fp = K.sum(((1 - y_true) * y_pred))
-    fn = K.sum((y_true * (1 - y_pred)))
-
-    tversky = (tp + smooth) / (tp + alpha * fp + beta * fn + smooth)
-    focal_tversky = K.pow((1 - tversky), gamma)
-
-    return focal_tversky
+    def __init__(self, alpha, beta, gamma, smooth=1e-6):
+        self.alpha = alpha
+        self.beta = beta
+        self.gamma = gamma
+        self.smooth = smooth
+    
+    def __call__(self, y_true, y_pred, sample_weight=None):
+        y_true = K.flatten(y_true)
+        y_pred = K.flatten(y_pred)
+        
+        # true pos, false pos, false neg
+        tp = K.sum((y_pred * y_true))
+        fp = K.sum(((1 - y_true) * y_pred))
+        fn = K.sum((y_true * (1 - y_pred)))
+        
+        tversky = (tp + self.smooth) / (tp + self.alpha * fp + self.beta * fn + self.smooth)
+        focal_tversky = K.pow((1 - tversky), self.gamma)
+        
+        return focal_tversky
