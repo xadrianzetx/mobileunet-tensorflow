ab21735c1cca9cfe69ac924d60b9d24258392419
Adrian Zuber <xadrianzetx@gmail.com>
2019-10-26 20:56:07 +0200

    config done right

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 import os
 import json
-import config
+import yaml
 import argparse
 import tensorflow as tf
 from utils.dataset import CULaneImageGenerator
@@ -26,19 +26,26 @@ def train():
         'model-name': os.environ['--model-name'],
         'checkpoint-name': os.environ['--checkpoint']
     }
+
+    with open('config.yaml', 'r') as file:
+        config = yaml.load(file)
     
     if args['mode'] == 'train' or args['mode'] == 'debug':
         # new model instance
-        model = MobileUNet(mode='binary', input_shape=config.IMG_SIZE, train_encoder=config.TRAIN_ENCODER).build()
-        loss = focal_tversky_loss(alpha=config.LOSS_ALPHA, beta=config.LOSS_BETA, gamma=config.LOSS_GAMMA)
-        optimizer = tf.keras.optimizers.Adam(learning_rate=config.LR)
+        input_shape = tuple(config['shapes']['image'])
+        train_enc = config['training']['train_encoder']
+        l_params = config['loss']
+
+        model = MobileUNet(mode='binary', input_shape=input_shape, train_encoder=train_enc).build()
+        loss = focal_tversky_loss(alpha=l_params['alpha'], beta=l_params['beta'], gamma=l_params['gamma'])
+        optimizer = tf.keras.optimizers.Adam(learning_rate=config['training']['lr'])
 
         metrics = [tf.keras.metrics.MeanIoU(num_classes=2), tf.keras.metrics.Precision(), dice_coefficient()]
         model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
     
     else:
         # load from checkpoint
-        path = os.path.join(config.LOAD_PATH, args['checkpoint-name'])
+        path = os.path.join(config['paths']['checkpoint'], args['checkpoint-name'])
         custom_obj = {
             'focal_tversky': focal_tversky_loss(alpha=0.7, beta=0.3, gamma=0.75), 
             'dice': dice_coefficient()
@@ -46,8 +53,8 @@ def train():
         model = tf.keras.models.load_model(path, custom_objects=custom_obj)
 
     # callbacks
-    tensorboard = tf.keras.callbacks.TensorBoard(log_dir=config.LOGDIR, update_freq='batch')
-    model_checkpoint = tf.keras.callbacks.ModelCheckpoint(filepath=config.SAVE_PATH)
+    tensorboard = tf.keras.callbacks.TensorBoard(log_dir=config['paths']['logs'], update_freq='batch')
+    model_checkpoint = tf.keras.callbacks.ModelCheckpoint(filepath=config['paths']['checkpoint'])
     reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(factor=0.2, patience=5, min_lr=0.0001)
 
     if args['mode'] == 'debug':
@@ -57,31 +64,35 @@ def train():
 
         train_g = CULaneImageGenerator(
             path=args['data-train'],
-            lookup_name=config.DEBUG_TRAIN_LOOKUP,
-            batch_size=config.DEBUG_BATCH_SIZE,
-            size=config.IMG_SIZE,
+            lookup_name=config['lookups']['debug'],
+            batch_size=config['batch']['debug_size'],
+            size=tuple(config['shapes']['image']),
             augment=True,
-            augmentations=config.AUGMENTATIONS
+            augmentations=config['training']['augmentations']
         )
 
         valid_g = CULaneImageGenerator(
             path=args['data-train'],
-            lookup_name=config.DEBUG_TRAIN_LOOKUP,
-            batch_size=config.DEBUG_BATCH_SIZE,
-            size=config.IMG_SIZE,
+            lookup_name=config['lookups']['debug'],
+            batch_size=config['batch']['debug_size'],
+            size=tuple(config['shapes']['image']),
             augment=False
         )
+
+        # generator output shapes
+        image_out = tuple(config['shapes']['debug_image_generator'])
+        mask_out = tuple(config['shapes']['debug_mask_generator'])
 
         train_generator = tf.data.Dataset.from_generator(
             generator=train_g,
             output_types=(tf.float16, tf.float16),
-            output_shapes=(config.DEBUG_GEN_IMG_OUT_SHAPE, config.DEBUG_GEN_MASK_OUT_SHAPE)
+            output_shapes=(image_out, mask_out)
         )
 
         valid_generator = tf.data.Dataset.from_generator(
             generator=valid_g,
             output_types=(tf.float16, tf.float16),
-            output_shapes=(config.DEBUG_GEN_IMG_OUT_SHAPE, config.DEBUG_GEN_MASK_OUT_SHAPE)
+            output_shapes=(image_out, mask_out)
         )
 
     else:
@@ -90,31 +101,35 @@ def train():
 
         train_g = CULaneImageGenerator(
             path=args['data-train'],
-            lookup_name=config.TRAIN_LOOKUP,
-            batch_size=config.BATCH_SIZE,
-            size=config.IMG_SIZE,
+            lookup_name=config['lookups']['train'],
+            batch_size=config['batch']['size'],
+            size=tuple(config['shapes']['image']),
             augment=True,
-            augmentations=config.AUGMENTATIONS
+            augmentations=config['training']['augmentations']
         )
 
         valid_g = CULaneImageGenerator(
             path=args['data-train'],
-            lookup_name=config.VALID_LOOKUP,
-            batch_size=config.BATCH_SIZE,
-            size=config.IMG_SIZE,
+            lookup_name=config['lookups']['valid'],
+            batch_size=config['batch']['size'],
+            size=tuple(config['shapes']['image']),
             augment=False
         )
+
+        # generator output shapes
+        image_out = tuple(config['shapes']['image_generator'])
+        mask_out = tuple(config['shapes']['mask_generator'])
 
         train_generator = tf.data.Dataset.from_generator(
             generator=train_g,
             output_types=(tf.float16, tf.float16),
-            output_shapes=(config.GEN_IMG_OUT_SHAPE, config.GEN_MASK_OUT_SHAPE)
+            output_shapes=(image_out, mask_out)
         )
 
         valid_generator = tf.data.Dataset.from_generator(
             generator=valid_g,
             output_types=(tf.float16, tf.float16),
-            output_shapes=(config.GEN_IMG_OUT_SHAPE, config.GEN_MASK_OUT_SHAPE)
+            output_shapes=(image_out, mask_out)
         )
     
     # train model
@@ -127,8 +142,8 @@ def train():
         )
     
     # save model and training log
-    model.save('{}.h5'.format(os.path.join(config.SAVE_PATH, args['model-name'])))
-    logpath = os.path.join(config.LOGDIR, args['model-name'])
+    model.save('{}.h5'.format(os.path.join(config['paths']['checkpoint'], args['model-name'])))
+    logpath = os.path.join(config['paths']['logs'], args['model-name'])
 
     with open('{}_logs.json'.format(logpath), 'w') as file:
         log = jsonify_history(history.history)
