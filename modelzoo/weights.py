4bedc549df9775c6331b43b57d45aa7da4bd0fda
Adrian Zuber <xadrianzetx@gmail.com>
2020-03-22 18:03:07 +0100

    add deeplabv3 pretrained weights

---
+++ modelzoo/weights.py
@@ -0,0 +1,14 @@
+class DeepLabV3PlusWeights:
+    """
+    Kudos to https://github.com/bonlime
+    """
+    NAME_CS = 'cityscapes'
+    PARAM_CS = {
+        'fname': 'deeplabv3_mobilenetv2_tf_dim_ordering_tf_kernels_cityscapes.h5',
+        'origin': 'https://github.com/bonlime/keras-deeplab-v3-plus/releases/download/1.2/deeplabv3_mobilenetv2_tf_dim_ordering_tf_kernels_cityscapes.h5'
+    }
+    NAME_VOC = 'pascal_voc'
+    PARAM_VOC = {
+        'fname': 'deeplabv3_mobilenetv2_tf_dim_ordering_tf_kernels.h5',
+        'origin': 'https://github.com/bonlime/keras-deeplab-v3-plus/releases/download/1.1/deeplabv3_mobilenetv2_tf_dim_ordering_tf_kernels.h5',
+    }
